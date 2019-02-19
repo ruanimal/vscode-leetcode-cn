043ce1c88be0b5b5692934b802ac403a2a27ca91
jayruan(阮连杰) <jayruan@tencent.com>
2019-02-19 14:14:53 +0800

    done: 1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -32,10 +32,11 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    return [i, j]
+        tmp_dict = {e: index for index, e in enumerate(nums)}
+        for index, e in enumerate(nums):
+            t = target - e
+            if t in tmp_dict and tmp_dict[t] != index:
+                return [index, tmp_dict[t]]
 
 if __name__ == "__main__":
     a = Solution().twoSum([2, 7, 11, 15], 9)
