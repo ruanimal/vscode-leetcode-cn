6af4625bce143b7ce867099809e400aa56e5eeb2
jayruan <jayruan@tencent.com>
2019-04-28 20:49:10 +0800

    add 3

---
+++ 999.由斜杠划分区域.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=999 lang=python
+#
+# [999] 由斜杠划分区域
+#
+class Solution(object):
+    def numRookCaptures(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        
+
