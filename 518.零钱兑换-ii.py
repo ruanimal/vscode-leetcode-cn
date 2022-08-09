723e472670eb132acee714dd19a4ca0ecca72ef6
ruan.lj <ruan.lj@foxmail.com>
2022-08-09 23:52:00 +0800

    M  518.零钱兑换-ii.py

--- 518.零钱兑换-ii.py
+++ 518.零钱兑换-ii.py
@@ -59,12 +59,13 @@
 #
 #
 
+from functools import lru_cache
 from comm import *
 # @lc code=start
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        """动态规划, 背包问题"""
+        """动态规划, 背包问题, 抄的答案"""
 
         # 以amount=10, coins=[2,3] 为例
         dp = [0] * (amount + 1)    # dp[x]: 拼成x的组合数
@@ -75,8 +76,102 @@ class Solution:
                     dp[i + coin] = dp[i + coin] + dp[i]   # 用这个coin的情况 + 不用这个coin的情况
         return dp[amount]
 
+class SolutionA:
+    def change(self, amount: int, coins: List[int]) -> int:
+        """动态规划其实就是分类递归, 该解法复杂度还是太高
+
+        dp[i][j]: 凑成i元时, 最后一个硬币面值是coins[j]的组合个数
+
+        如, 金额为5时, 可选硬币为[1, 2, 5]
+           5=5
+           5=2+2+1
+           5=2+1+1+1
+           5=1+1+1+1+1
+
+        即dp[5][1] = 3 = dp[4][1] + dp[4][2] + dp[4][5] = 2 + 1 + 0
+          dp[5][2] = 0 = dp[3][2] + dp[3][5] = 0 + 0
+          dp[5][5] = 1
+
+
+        对于每种硬币j, 只要dp[i-coins[j]]的所有组合结尾类型k满足 coins[k] >= coins[j]
+        则都可以加上一个硬币j凑成金额j, (大于等于的条件是为了不重复, 其实k>=j也是一样的)
+
+        """
+
+        dp = [[0 for _ in coins] for _ in range(amount+1)]
+        for idx, i in enumerate(coins):
+            if i <= amount:
+                dp[i][idx] = 1
+
+        for i in range(amount+1):
+            for j in range(len(coins)):
+                for k in range(j, len(coins)):
+                    if i > coins[j]:
+                        dp[i][j] += dp[i-coins[j]][k]
+        # pprint(dp)
+        return sum(dp[amount])
+
+class SolutionB:
+    def change(self, amount: int, coins: List[int]) -> int:
+        """
+        接着SolutionA, 从公式的累加可以看出, 这个存在一个后缀和优化方法
+
+        可以把dp数组的定义改成
+
+        dp[i][j]: 凑成i元时, 最后一个硬币面值大于等于coins[j]的组合个数
+        则
+            dp[i][j] = dp[i-coins[j]][j]
+        """
+
+        if amount == 0:
+            return 1
+
+        dp = [[0 for _ in coins] for _ in range(amount+1)]
+        for idx, i in enumerate(coins):
+            if i <= amount:
+                dp[i][idx] = 1
+
+        for i in range(amount+1):
+            pre_val = 0
+            for j in range(len(coins)-1, -1, -1):
+                if i > coins[j]:
+                    dp[i][j] += dp[i-coins[j]][j] + pre_val
+                else:
+                    dp[i][j] += pre_val
+                pre_val = dp[i][j]
+        # print(dp)
+        return dp[amount][0]
+
+class SolutionC:
+    def change(self, amount: int, coins: List[int]) -> int:
+        """
+        接着SolutionB, 发现如果改变遍历顺序, 就可以少保留一个状态
+
+        用整个dp数组代表pre_val
+        """
+
+        if amount == 0:
+            return 1
+
+        dp = [0 for _ in range(amount+1)]
+        for j in range(len(coins)-1, -1, -1):
+            for i in range(amount+1):
+                if i == coins[j]:
+                    dp[i] += 1
+                elif i > coins[j]:
+                    dp[i] += dp[i-coins[j]]
+        return dp[amount]
 # @lc code=end
 
 if __name__ == "__main__":
-    s = Solution().change(amount = 10, coins = [1, 2, 5])
+    s = Solution().change(amount = 5, coins = [1, 2, 5])
     print(s)
+    s = SolutionA().change(amount = 5, coins = [1, 2, 5])
+    print(s)
+    s = SolutionB().change(amount = 5, coins = [1, 2, 5])
+    print(s)
+    s = SolutionC().change(amount = 100, coins = [99, 1])
+    print(s)
+    s = SolutionC().change(amount = 5, coins = [1, 2, 5])
+    print(s)
+
