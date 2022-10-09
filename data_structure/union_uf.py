7445fe5d0380f0bb68af5f0075e8792cac61207b
ruan.lj <ruan.lj@foxmail.com>
2022-10-09 11:43:12 +0800

    M  data_structure/union_uf.py

--- data_structure/union_uf.py
+++ data_structure/union_uf.py
@@ -2,24 +2,30 @@
 # -*- coding:utf-8 -*-
 
 
-class QuickUnionUF:
+class UnionFind:
     def __init__(self, n):
-        self.roots = [i for i in range(n)]
+        self.parent = [i for i in range(n)]  # parent[i] = j 表示i节点的根节点是j
+        self.count = n
 
-    def findRoot(self, i):
-        root = i
-        while root != self.roots[root]:
-            root = self.roots[root]
-        while i != self.roots[i]:
-            tmp = self.roots[i]
-            self.roots[i] = root
-            i = tmp
-        return root
+    def find(self, p):
+        if self.parent[p] != p:   # 不是根节点
+            self.parent[p] = self.find(self.parent[p])   # 路径折叠
+        return self.parent[p]
 
     def connected(self, p, q):
-        return self.findRoot(p) == self.findRoot(q)
+        return self.find(p) == self.find(q)
 
     def union(self, p, q):
-        pr = self.findRoot(p)
-        qr = self.findRoot(q)
-        self.roots[pr] = qr
+        pr = self.find(p)
+        qr = self.find(q)
+        if pr == qr:    # 本来就在同一个连通分量中
+            return
+        self.parent[pr] = qr
+        self.count -= 1
+
+
+uf = UnionFind(10)
+uf.union(1,2)
+print(uf.parent)
+uf.union(1,7)
+print(uf.parent)
